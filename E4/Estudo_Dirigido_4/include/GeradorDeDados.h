26d395c2ecc6ea1c6c1900e41689fe11aa70c440
Felipe Santos <felipesantos956@poli.ufrj.br>
2017-11-18 16:43:20 -0200

    Adicionado mais comentários

--- E4/Estudo_Dirigido_4/include/GeradorDeDados.h
+++ E4/Estudo_Dirigido_4/include/GeradorDeDados.h
@@ -20,10 +20,13 @@ class GeradorDeDados
         //numeroElementos eh o numero de total de elementos no vetor que sera gerado
         //numeroInicial eh o numero minimo permitido
         //numeroFinal eh o numero maximo permitido
+        //OBS: nao colocar numero de elementos maior que 10,
+        //pois a funcao nao conseguira gerar o numero
         unsigned gerarNumeros (unsigned numeroElementos,
                                unsigned numeroInicial,
                                unsigned numeroFinal);
 
+        //Adiciona o prefixo ao numero de entrada e retorna tudo como string
         string GerarTelefone (string prefixo, unsigned numero);
 
         virtual ~GeradorDeDados();
