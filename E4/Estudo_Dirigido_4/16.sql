68d9d65a4997e1c4abaf2ce53afd75636f4d5ddf
Felipe Santos <felipesantos956@poli.ufrj.br>
2017-11-26 23:16:38 -0200

    Consertando o merge anterior

--- E4/Estudo_Dirigido_4/16.sql
+++ E4/Estudo_Dirigido_4/16.sql
@@ -1,10 +1,3 @@
-SELECT COUNT( Nu_Dre),Turma.Nu_SIAPE FROM Inscricao
-INNER JOIN Turma ON Turma.Nu_Turma = Inscricao.Nu_Turma
-WHERE Inscricao.Nu_Dre IN (SELECT Nu_Dre FROM Inscricao
-							INNER JOIN Turma ON Turma.Nu_Turma = Inscricao.Nu_Turma
-							WHERE Turma.Nu_SIAPE = 143221535
-							)
-AND Turma.Nu_SIAPE = 122972907;
 SELECT Aluno.Nu_dre, Pessoa.Nm_Pessoa, Turma.Cd_Periodo
 FROM Aluno
 INNER JOIN Pessoa ON Aluno.Cd_Pessoa = Pessoa.Cd_Pessoa
