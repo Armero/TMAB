f8e410dfc4c76156de1ea292686376d7d4ef862a
Felipe Santos <felipesantos956@poli.ufrj.br>
2017-11-26 22:49:39 -0200

    Queries 14, 15 e 16 feitas

--- E4/Estudo_Dirigido_4/15.sql
+++ E4/Estudo_Dirigido_4/15.sql
@@ -4,7 +4,7 @@ INNER JOIN Periodo ON Grade_Curricular.Cd_Periodo = Periodo.Cd_Periodo
 INNER JOIN Disciplina ON Grade_Curricular.Cd_Disciplina = Disciplina.Cd_Disciplina
 WHERE Periodo.Cd_Periodo = 4
 
-AND Grade_Curricular.Cd_Disciplina IN (
+AND Grade_Curricular.Cd_Disciplina NOT IN (
 
 SELECT Grade_Curricular.Cd_Disciplina 
 FROM Grade_Curricular
---
+++ E4/Estudo_Dirigido_4/16.sql
@@ -0,0 +1,17 @@
+SELECT Aluno.Nu_dre, Pessoa.Nm_Pessoa 
+FROM Aluno
+INNER JOIN Pessoa ON Aluno.Cd_Pessoa = Pessoa.Cd_Pessoa
+INNER JOIN Inscricao ON Inscricao.Nu_Dre = Aluno.Nu_Dre
+INNER JOIN Turma ON Inscricao.Nu_Turma = Turma.Nu_Turma
+INNER JOIN Professor ON Turma.Nu_SIAPE = Professor.Nu_SIAPE
+WHERE Professor.Cd_Pessoa = 1
+
+AND Aluno.Nu_dre IN (
+
+SELECT Aluno.Nu_dre
+FROM Aluno
+INNER JOIN Pessoa ON Aluno.Cd_Pessoa = Pessoa.Cd_Pessoa
+INNER JOIN Inscricao ON Inscricao.Nu_Dre = Aluno.Nu_Dre
+INNER JOIN Turma ON Inscricao.Nu_Turma = Turma.Nu_Turma
+INNER JOIN Professor ON Turma.Nu_SIAPE = Professor.Nu_SIAPE
+WHERE Professor.Cd_Pessoa = 2 );
