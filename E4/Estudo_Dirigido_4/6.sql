0ea48eecb9fe9857ec72126932bcfea40aa44282
Felipe Santos <felipesantos956@poli.ufrj.br>
2017-11-26 23:44:36 -0200

    Query 6 OK
    Depende muito de sorte para acertar uma query que não retorne vazio

---
+++ E4/Estudo_Dirigido_4/6.sql
@@ -0,0 +1,8 @@
+SELECT Disciplina.Cd_Disciplina, Disciplina.Nm_Disciplina, Grade_Curso.Ic_Periodo, 
+Grade_Curso.Cd_Curso, Turma.Cd_Periodo
+FROM ALUNO
+INNER JOIN Grade_Curso ON Aluno.Cd_Curso = Grade_Curso.Cd_Curso
+AND Grade_Curso.Ic_Periodo = 1
+INNER JOIN Disciplina ON Grade_Curso.Cd_Disciplina = Disciplina.Cd_Disciplina
+INNER JOIN Turma ON Turma.Cd_Disciplina = Disciplina.Cd_Disciplina
+WHERE Aluno.Cd_Pessoa = 350 AND Turma.Cd_Periodo = 4;
